7b7eedc37b47a2c916c100c4c0d98092338cd7c9
bobrock <barret@tarkmail.com>
2022-03-25 16:50:09 -0500

    Trying to get gunicorn to find this

--- run_debug.py
+++ run_debug.py
@@ -1,8 +1,8 @@
-from davaiops.configurations import DevelopmentConfig
 # Internal packages
+from davaiops.configurations import DevelopmentConfig
 from davaiops.routes import create_app
 
+app = create_app(config_class=DevelopmentConfig)
 
 if __name__ == "__main__":
-    app = create_app(config_class=DevelopmentConfig)
     app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 # Internal packages
+from davaiops.configurations import ProductionConfig
 from davaiops.routes import create_app
 
+app = create_app(config_class=ProductionConfig)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run()
