a6ab8aff2a64c18d1ad199ed75e4de833bb19659
bobrock <barret@tarkmail.com>
2022-04-20 17:38:04 -0500

    Update to new structure

--- _auto_config.sh
+++
@@ -1,9 +0,0 @@
-# **This is an automatically-constructed file. See config.py for the source.**
-
-REPO_NAME="davaiops"
-REPO_DIR="${HOME}/extras/davaiops"
-GIT_URL="git+https://github.com/barretobrock/davaiops.git#egg=davaiops"
-VENV_PATH="${HOME}/venvs/davaiops/bin/python3"
-DEP_LINKS=(
-)
-MAIN_BRANCH="main"
--- davaiops/__init__.py
+++ davaiops/__init__.py
@@ -1,3 +1,5 @@
 
-from . import _version
-__version__ = _version.get_versions()['version']
+__version__ = '1.0.0'
+__update_date__ = '2022-04-20_16:41:22'
+from pathlib import Path
+ROOT_PATH = Path(__file__)
